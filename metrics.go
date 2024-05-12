46ad6903ca15751ec2c3bf285dd09d21a2f71f9c
kalinchkma <chakma.kalin10211@gmail.com>
2024-05-13 02:18:36 +0600

    added comment to a file

--- metrics.go
+++ metrics.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// metrics functions
+
 func (c *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
