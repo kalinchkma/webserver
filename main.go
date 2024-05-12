8c692a470223d6c5e2562e0c1f89f5fc64bd8664
kalinchkma <chakma.kalin10211@gmail.com>
2024-05-13 02:15:20 +0600

    code splited into different file

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -37,16 +36,3 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func (c *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", c.fileServerHits)))
-}
-
-func (c * apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		c.fileServerHits++
-		fmt.Println("Hits %s", c.fileServerHits)
-		next.ServeHTTP(w, r)
-	})
-}
---
+++ metrics.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (c *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", c.fileServerHits)))
+}
+
+func (c * apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileServerHits++
+		fmt.Println("Hits %s", c.fileServerHits)
+		next.ServeHTTP(w, r)
+	})
+}
