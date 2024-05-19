a6dc619043f51b76fbef683becf12f56b9610c84
kalinchkma <chakma.kalin10211@gmail.com>
2024-05-19 23:57:16 +0600

    actix added to the project

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
-use yew::prelude::*;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
-    }
+struct AppState {
+    app_name: String
 }
 
-fn main() {
-    yew::Renderer::<App>::new().render();
+#[get("/hello-world")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
 }
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("This is the cool name of all time {app_name}")
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Manual hello world")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+         .app_data(web::Data::new(AppState {
+            app_name: String::from("Popcorn")
+         })).service(index)
+         .service(hello)
+         .service(
+            web::scope("/api")
+            .route("/manual", web::get().to(manual_hello))
+         )
+    }).bind(("127.0.0.1", 7878))?
+    .run()
+    .await
+}
+
